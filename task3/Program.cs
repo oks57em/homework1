78149a06f96771104452294329434bc61356232b
oks57em <eminovaoksana81@gmail.com>
2023-03-14 16:04:46 +0300

    задача 6

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// написать программу,на вход число и выдает четное или нет
+//4 ->да
+//3 ->нет
+//7 ->нет
+Console.WriteLine("ввести число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number==0)
+Console.WriteLine("введено 0,введите правильное число");
+else if(number%2==0)
+Console.WriteLine("число четное:");
+else
+Console.WriteLine("число нечетное:");
+
+    
+
