820d6e8c2ff112e52ed561e3c9f97d4a3a5662e7
oks57em <eminovaoksana81@gmail.com>
2023-03-14 16:21:13 +0300

    задача 8

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// написать программу,на вход число(N),на выходе 
+//показать все четные от 1 до N
+//5 ->2,4
+//8 ->2,4,6,8
+Console.WriteLine("ввести число:");
+int number=Convert.ToInt32(Console.ReadLine());
+int i=2;
+if (number<2)
+Console.WriteLine("число четное:");
+else
+   while (i<=number)
+   {
+    Console.WriteLine(i+"/");
+    i+=2;
+   }
+
+    
+
+
+    
+
