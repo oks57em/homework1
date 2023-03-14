742a9517cea2e5478da21005600461fe7877a394
oks57em <eminovaoksana81@gmail.com>
2023-03-14 22:32:37 +0300

    задача 13

---
+++ task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// напишите программу,которая выводит третью цифру
+// заданного числа или сообщает,что третьей цифры нет
+//645->5 78->третьей нет 
+Console.WriteLine("введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>1000)
+{
+  while (number>999)
+  {
+    number/=10;
+  }
+    Console.WriteLine(number%10);
+   } else{
+    Console.WriteLine(number%10);
+    }
+        
+    
+
+    
+
