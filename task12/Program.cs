391d3f32f10630865bc6f448b43e8abddb70e050
oks57em <eminovaoksana81@gmail.com>
2023-03-17 19:41:46 +0300

    задача 27

---
+++ task12/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//программа , которая принимает на вход число и выдает
+// сумму цифр в числе 
+//4 5 2->11  8 2->10  9 0 1 2 -> 12
+Console.Write("Введите число:");
+int number =Convert.ToInt32(Console.ReadLine());
+
+int sum =0;
+while( number>0)
+{
+     int digit = number%10;
+     sum += digit;
+     number/=10;
+}
+Console.WriteLine("Сумма цифр числа равна:"+sum);
