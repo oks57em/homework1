f2b914dbf5b37d51da24f6b01a86bd80b78a6d3f
oks57em <eminovaoksana81@gmail.com>
2023-03-20 11:52:17 +0300

    задача 38

---
+++ task16/Program.cs
@@ -0,0 +1,38 @@
+﻿// задайте массив вещественных чисел.Найдите разницу
+//между минимальными максимальным элементом массива.
+//[37 22 2 78]->76
+double[]array={3.14,7.22,2,78};
+
+double max=array[0];
+double min=array[0];
+
+foreach(double number in array)
+{
+  if(number>max)
+{
+max=number;
+}
+if(number<min)
+{
+min=number;
+}
+}
+double diff=max-min;
+Console.WriteLine("Массив:");
+foreach(double number in array)
+{
+  Console.WriteLine(number+"");
+}
+Console.WriteLine();
+Console.WriteLine("Разница между максимальным и минимальным элементами:"+diff);
+
+
+
+
+
+
+
+   
+      
+      
+      
