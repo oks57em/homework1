24f5d03d347dcbeb32273e99b61e4640373b5e50
oks57em <eminovaoksana81@gmail.com>
2023-03-23 20:00:50 +0300

    задача 54

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,107 +1,107 @@
 ﻿// Задайте двумерный массив.Напишите программу,которая 
 // упорядочит по убыванию элементы каждой строки 
 // двумерного массива.
-//  Console.WriteLine("Введите количество строк");
-// int linesVol=Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите количество столбцов");
-// int columnsVol=Convert.ToInt32(Console.ReadLine());
-// int[,]numbers =new int[linesVol,columnsVol];
-// FillArrayRandomNumbers(numbers);
-// Console.WriteLine();
-// Console.WriteLine("Массив до изменения");
-// PrintArray(numbers);
+Console.WriteLine("Введите количество строк");
+int linesVol=Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите количество столбцов");
+ int columnsVol=Convert.ToInt32(Console.ReadLine());
+ int[,]numbers =new int[linesVol,columnsVol];
+ FillArrayRandomNumbers(numbers);
+ Console.WriteLine();
+ Console.WriteLine("Массив до изменения");
+ PrintArray(numbers);
 
-// for(int i=0; i< numbers.GetLength(0); i++)
-// {  
-//     for(int j=0; j< numbers.GetLength(1)-1 ;j++)
-//     {
-//         for(int z=0; z< numbers.GetLength(1)-1; z++)
-//         {
-//            if(numbers[i,z]<numbers[i,z+1])
-//            {
-//               int temp =0;
-//               temp = numbers[i,z];
-//               numbers[i,z ]=numbers[i,z+1];
-//               numbers[i,z+1]=temp;
-//             }
-//         }
-//     }
-// }
-// Console.WriteLine();
-// Console.WriteLine("Массив с упорядоченными значениями");
-// PrintArray(numbers);
+ for(int i=0; i< numbers.GetLength(0); i++)
+ {  
+     for(int j=0; j< numbers.GetLength(1)-1 ;j++)
+     {
+        for(int z=0; z< numbers.GetLength(1)-1; z++)
+         {
+            if(numbers[i,z]<numbers[i,z+1])
+            {
+               int temp =0;
+               temp = numbers[i,z];
+               numbers[i,z ]=numbers[i,z+1];
+               numbers[i,z+1]=temp;
+            }
+         }
+     }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(numbers);
 
-// void FillArrayRandomNumbers(int[,]array)
-// {
-//     for(int i=0;i<array.GetLength(0);i++)
-//     {
-//         for(int j =0; j<array.GetLength(1);j++)
-//         {
-//             array[i,j]=new Random().Next(0,10);
-//         }
-//     }
-// }
-// void PrintArray(int[,] array)
-// {
-//      for (int i=0;i< array.GetLength(0);i++)
-//      {
-//          Console.Write("[");
-//          for(int j=0;j<array.GetLength(1);j++)
-//          {
-//             Console.Write(array[i,j]+"");
-//          }
-//          Console.Write("]");
-//          Console.WriteLine("");
-//     }
-// }
+ void FillArrayRandomNumbers(int[,]array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+     {
+         for(int j =0; j<array.GetLength(1);j++)
+        {
+            array[i,j]=new Random().Next(0,10);
+         }
+     }
+ }
+void PrintArray(int[,] array)
+ {
+      for (int i=0;i< array.GetLength(0);i++)
+     {
+         Console.Write("[");
+          for(int j=0;j<array.GetLength(1);j++)
+          {
+             Console.Write(array[i,j]+"");
+          }
+          Console.Write("]");
+          Console.WriteLine("");
+     }
+ }
 
  //задайте прямоугольный двумерный массив.Напишите программу
  // которая будет находить строку с наименьшей 
  //  суммой элементов.
-// Console.WriteLine("Введите размер квадратного массива");
-// int massVol =Convert.ToInt32(Console.ReadLine()); 
-// int[,] numbers=new int[massVol,massVol];
-// FillArrayRandomNumbers(numbers);
-// PrintArray(numbers);
-// int minsum =Int32.MaxValue;
-// int indexLine=0;
+Console.WriteLine("Введите размер квадратного массива");
+int massVol =Convert.ToInt32(Console.ReadLine()); 
+int[,] numbers=new int[massVol,massVol];
+FillArrayRandomNumbers(numbers);
+ PrintArray(numbers);
+ int minsum =Int32.MaxValue;
+ int indexLine=0;
 
-// for(int i=0; i< numbers.GetLength(0); i++)
-// {
-//     int sum =0;
-//     for(int j=0; j< numbers.GetLength(1);j++)
-//     {
-//        sum = sum + numbers[indexLine,j];
-//     }
-//     if(sum< minsum)
-//     indexLine++;
-//     }
+ for(int i=0; i< numbers.GetLength(0); i++)
+ {
+     int sum =0;
+     for(int j=0; j< numbers.GetLength(1);j++)
+    {
+        sum = sum + numbers[indexLine,j];
+     }
+     if(sum< minsum)
+     indexLine++;
+     }
 
-// Console.WriteLine("Строка с наименьшей суммой элементов под номером:"+(indexLine)+",с суммой элементов равной:"+(minsum));
+ Console.WriteLine("Строка с наименьшей суммой элементов под номером:"+(indexLine)+",с суммой элементов равной:"+(minsum));
 
-// void FillArrayRandomNumbers(int[,]array)
-// {
-//      for(int i=0;i<array.GetLength(0);i++)
-//      {
-//          for(int j=0;j<array.GetLength(1);j++)
-//          {
-//              array[i,j]=new Random().Next(0,10);
-//         }
-//     }
-// }
-// void PrintArray(int[,]array)
-// {
-//      for(int i=0;i<array.GetLength(0);i++)
-//      {
-//         Console.Write("[");
-//         for(int j=0;j<array.GetLength(1);j++)
-//         {
-//             Console.Write(array[i,j]+"");
-//         }
-//         Console.Write("]");
-//         Console.WriteLine("");
-//     }
-// }
+ void FillArrayRandomNumbers(int[,]array)
+ {
+      for(int i=0;i<array.GetLength(0);i++)
+      {
+          for(int j=0;j<array.GetLength(1);j++)
+          {
+              array[i,j]=new Random().Next(0,10);
+         }
+     }
+ }
+ void PrintArray(int[,]array)
+ {
+      for(int i=0;i<array.GetLength(0);i++)
+     {
+         Console.Write("[");
+         for(int j=0;j<array.GetLength(1);j++)
+         {
+             Console.Write(array[i,j]+"");
+         }
+        Console.Write("]");
+         Console.WriteLine("");
+     }
+ }
 //Задайте две матрицы.Напишите программу,которая будет
 // находить произведение двух матриц.
 int InputInt(string output)
