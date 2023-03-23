faf9d4ff912e7126eb89bac6f5b7a61970abbe5c
oks57em <eminovaoksana81@gmail.com>
2023-03-23 22:33:27 +0300

    задача 66

---
+++ task28/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение M и N .Напишите программу,которая найдет
+// сумму натуральных элементов в промежутке от M до N.
+Console.Write("введите значение M:");
+int m= Convert.ToInt32(Console.ReadLine());
+Console.Write("введите значение N:");
+int n= Convert.ToInt32(Console.ReadLine());
+int sum(int m,int n)
+{
+  if(m==n)
+  return n;
+    else return m+ sum (m+1,n);
+  }
+Console.Write($"сумма натуральных элементов в промежутке от M до N {sum(m,n)}");
